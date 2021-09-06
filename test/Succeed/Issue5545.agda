liesnikov/agda

commit c2f023513f8823c65496b13d991d43b4b47b5ae5

[ fix #5291 ] lexer lookAhead: proper longest match procedure
If matching gets stuck, fallback to the longest match found so far instead of failing.

src/full/Agda/Syntax/Parser/LookAhead.hs
@@ -24,6 +24,8 @@ import Control.Monad.State
 import Agda.Syntax.Parser.Alex
 import Agda.Syntax.Parser.Monad
 
+import Agda.Utils.Null (ifNull)
+
 {--------------------------------------------------------------------------
     The look-ahead monad
  --------------------------------------------------------------------------}
@@ -113,17 +115,42 @@ match xs def =
     the input is not advanced.
 -}
 match' :: Char -> [(String, LookAhead a)] -> LookAhead a -> LookAhead a
-match' c xs def =
-    do  inp <- getInput
-        match'' inp xs c
-    where
-        match'' inp bs c =
-            case bs' of
-                []          -> setInput inp >> def
-                [("",p)]    -> p
-                _           -> match'' inp bs' =<< nextChar
-            where
-                bs' = [ (s, p) | (c':s, p) <- bs, c == c' ]
+match' c xs def = do
+
+  -- Set the error continuation to the default @def@, but make sure we reset
+  -- the input to where we started speculative matching.
+  inp <- getInput
+  let fallback = setInput inp >> def
+
+  -- Find the longest match from the table.
+  match'' fallback xs c
+
+  where
+  match'' fallback bs c =
+
+    -- Match the first character, dropping entries that do not match.
+    ifNull [ (s, p) | (c':s, p) <- bs, c == c' ]
+
+      -- If no alternatives are left, fall back to the failure continuation.
+      {-then-} fallback
+
+      -- Otherwise:
+      {-else-} $ \ bs' -> do
+
+        -- If we have a successful match, store it in the failure continuation.
+        fallback' <- do
+          case lookup "" bs' of
+
+            -- No match yet.
+            Nothing -> pure fallback
+
+            -- Match found!  Remember it, and the state of the input where we found it.
+            Just p  -> do
+              inp <- getInput
+              pure $ setInput inp >> p
+
+        -- Keep trying to find a (longer) match.
+        match'' fallback' bs' =<< nextChar
 
 -- | Run a 'LookAhead' computation. The first argument is the error function.
 runLookAhead :: (forall b. String -> LookAhead b) -> LookAhead a -> Parser a

test/Succeed/Issue5291.agda
@@ -1,0 +1,19 @@
+-- Andreas, 2021-08-19, issue #5291 reported by gergoerdi
+-- https://stackoverflow.com/q/66816547/477476
+
+open import Agda.Builtin.Char
+open import Agda.Builtin.String
+
+works : Char
+works = '\SOH'
+
+-- This used to fail as the matcher was committed to finding SOH after SO.
+-- (Worked only for prefix-free matching.)
+
+test : Char
+test = '\SO'
+
+-- Here, all the silly legacy ASCII escape sequences in their glorious detail...
+
+all : String
+all = "\NUL\SOH\STX\ETX\EOT\ENQ\ACK\BEL\BS\HT\LF\VT\FF\CR\SO\SI\DEL\DLE\DC1\DC2\DC3\DC4\NAK\SYN\ETB\CAN\EM\SUB\ESC\FS\GS\RS\US"

commit 7fbb2dc53a75008d17a4c50826f4585bf34f2250

[ fix #5545 ] lexer: commit to match at eof
Fixes regression introduced by #5522.

fix-whitespace.yaml
@@ -55,9 +55,10 @@ included-files:
 excluded-files:
 # Andreas (24 Sep 2014).
 # The following files are exempt from the whitespace check,
-# as they test behavior of Agda with regard to tab characters.
+# as they test behavior of Agda with regard to e.g. tab characters.
   - "test/Succeed/Whitespace.agda"
   - "test/Succeed/Issue1337.agda"
+  - "test/Succeed/Issue5545.agda"
   - "test/Fail/Tabs.agda"
   - "test/Fail/TabsInPragmas.agda"
   - "src/full/Agda/Syntax/Parser/Lexer.hs"

src/full/Agda/Syntax/Parser/Literate.hs
@@ -26,6 +26,9 @@ import Control.Monad ((<=<))
 import Data.Char (isSpace)
 import Data.List (isPrefixOf)
 import Text.Regex.TDFA
+  ( Regex, getAllTextSubmatches, match, matchM
+  , makeRegexOpts, blankCompOpt, blankExecOpt, newSyntax, caseSensitive
+  )
 
 import Agda.Syntax.Common
 import Agda.Syntax.Position

src/full/Agda/Syntax/Parser/LookAhead.hs
@@ -25,6 +25,7 @@ import Agda.Syntax.Parser.Alex
 import Agda.Syntax.Parser.Monad
 
 import Agda.Utils.Null (ifNull)
+import Agda.Utils.Maybe (caseMaybeM, fromMaybeM)
 
 {--------------------------------------------------------------------------
     The look-ahead monad
@@ -69,13 +70,17 @@ liftP = LookAhead . lift . lift
 
 -- | Look at the next character. Fails if there are no more characters.
 nextChar :: LookAhead Char
-nextChar =
+nextChar = fromMaybeM (lookAheadError "unexpected end of file") nextCharMaybe
+
+-- | Look at the next character. Return 'Nothing' if there are no more characters.
+nextCharMaybe :: LookAhead (Maybe Char)
+nextCharMaybe =
     do  inp <- getInput
         case alexGetChar inp of
-            Nothing         -> lookAheadError "unexpected end of file"
+            Nothing         -> return Nothing
             Just (c,inp')   ->
                 do  setInput inp'
-                    return c
+                    return $ Just c
 
 
 -- | Consume all the characters up to the current look-ahead position.
@@ -104,11 +109,11 @@ eatNextChar =
     move past it and execute the corresponding action. If no string matches, we
     execute a default action, advancing the input one character. This function
     only affects the look-ahead position.
+
+    If we are at the end of file, we just execute the default action.
 -}
 match :: [(String, LookAhead a)] -> LookAhead a -> LookAhead a
-match xs def =
-    do  c <- nextChar
-        match' c xs def
+match xs def = caseMaybeM nextCharMaybe def $ \ c -> match' c xs def
 
 {-| Same as 'match' but takes the initial character from the first argument
     instead of reading it from the input.  Consequently, in the default case
@@ -150,7 +155,7 @@ match' c xs def = do
               pure $ setInput inp >> p
 
         -- Keep trying to find a (longer) match.
-        match'' fallback' bs' =<< nextChar
+        maybe fallback' (match'' fallback' bs') =<< nextCharMaybe
 
 -- | Run a 'LookAhead' computation. The first argument is the error function.
 runLookAhead :: (forall b. String -> LookAhead b) -> LookAhead a -> Parser a

test/Succeed/Issue5545.agda
@@ -1,0 +1,13 @@
+-- Andreas, 2021-09-06, issue #5545,
+-- regression introduced by #5522 (fix for #5291).
+--
+-- Note: this test requires that there is no newline at the end of file.
+-- So, it violates the whitespace requirement of fix-whitespace,
+-- and needs to be an exception in the configuration of fix-whitespace.
+
+{-# OPTIONS --allow-unsolved-metas #-}
+
+-- No newline at end of file triggered "Unterminated {!" error.
+
+_ : Set
+_ = {! !}